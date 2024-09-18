14dbd42fdb8ac234cbc7beae78b4b2479b854a93
Ali Sencer Irmak <alisencer.irmak@std.yeditepe.edu.tr>
2024-09-18 14:42:49 +0300

    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { VatAddedPipe } from './pipes/vat-added.pipe';
 import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 
 import { ToastrModule } from 'ngx-toastr';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { ToastrModule } from 'ngx-toastr';
     BrandComponent,
     NaviComponent,
     VatAddedPipe,
-    FilterPipePipe
+    FilterPipePipe,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
